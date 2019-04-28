e51facde11fe298b9327c077d080d4a0bf7a9287
SnowPlumes <lzw0279@163.com>
2019-04-28 09:20:39 +0800

    gateway

--- cloud-customer/customer-user/src/main/java/me/lv/CustomerUserApplication.java
+++ cloud-customer/customer-user/src/main/java/me/lv/CustomerUserApplication.java
@@ -1,5 +1,7 @@
 package me.lv;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,7 +15,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class CustomerUserApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(CustomerUserApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerUserApplication.class, args);
+        logger.info(">>>>>> customer user application running ......");
     }
 }
